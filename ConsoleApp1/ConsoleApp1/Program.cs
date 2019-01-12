e5758b7f480e8b4f6b329766b6cfcd635ea9ec41
Bohdan Yakymenko <13876285+cossackjedi@users.noreply.github.com>
2019-01-12 15:32:09 +0200

    Added lines 2,3,4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new HtmlDocument();
+            //doc.Load();
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
